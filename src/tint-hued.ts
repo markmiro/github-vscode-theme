8fd008ed042068a1118aa8fbaacd8cf873f2b0a7
Mark Miro <contact@markmiro.com>
2025-05-03 22:10:34 -0700

    Add ansi to hued tinting

--- src/tint-hued.ts
+++ src/tint-hued.ts
@@ -17,5 +17,18 @@ export function tintHued(
   t.scale.pink = tintColor(t.scale.pink, tint);
   t.scale.coral = tintColor(t.scale.coral, tint);
 
+  t.ansi.red = tintColor(t.ansi.red, tint);
+  t.ansi.redBright = tintColor(t.ansi.redBright, tint);
+  t.ansi.green = tintColor(t.ansi.green, tint);
+  t.ansi.greenBright = tintColor(t.ansi.greenBright, tint);
+  t.ansi.yellow = tintColor(t.ansi.yellow, tint);
+  t.ansi.yellowBright = tintColor(t.ansi.yellowBright, tint);
+  t.ansi.blue = tintColor(t.ansi.blue, tint);
+  t.ansi.blueBright = tintColor(t.ansi.blueBright, tint);
+  t.ansi.magenta = tintColor(t.ansi.magenta, tint);
+  t.ansi.magentaBright = tintColor(t.ansi.magentaBright, tint);
+  t.ansi.cyan = tintColor(t.ansi.cyan, tint);
+  t.ansi.cyanBright = tintColor(t.ansi.cyanBright, tint);
+
   return t;
 }
